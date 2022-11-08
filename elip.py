8d424492edc2dd03cc25fff6a3f7c51a380e1a48
nguyetnong <nguyetnong71099@gmail.com>
2022-11-08 22:23:16 +0700

    ve hinh elip theo mau

---
+++ elip.py
@@ -0,0 +1,24 @@
+import turtle
+import random
+r=int(input("nhap ban kinh:"))
+mau=["red","blue","green","brown","pink"]
+turtle.speed(10)
+def draw(rad):
+
+      for i in range(2):
+        turtle.circle(rad,90)
+        turtle.circle(rad//2,90)
+    #turtle.seth(-45)
+cnt=0
+cnt_mau=0
+
+while cnt<50:
+    turtle.color(mau[cnt_mau])
+    # calling draw method
+    draw(100)
+    turtle.right(15)
+    cnt_mau +=1
+    if cnt_mau >= 5:
+        cnt_mau = 0
+    cnt+=1
+turtle.done()
